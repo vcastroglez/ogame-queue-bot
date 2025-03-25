d6de9878f256f0e98efb83857336c06d6eae2101
Sanji / Vladimir Castro <sanji@chibiphoenix.com>
2025-03-25 10:42:50 +0200

    tampermonkeyscript.js

---
+++ tampermonkeyscript.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      2025-02-28
+// @description  try to take over the world!
+// @author       You
+// @match        https://s261-es.ogame.gameforge.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=gameforge.com
+// @grant        none
+// @require file:///var/www/html/tamper.js
+// ==/UserScript==
